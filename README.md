e4f3feef2c7529cb3372948f386995df976c27ce
Ryan McMinds <r.mcminds@gmail.com>
2026-07-09 15:02:08 -0700

    create website with actions, quarto, and gh pages

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Welcome to the Pacific Northwest Tribal Coding Group Github repo!
 This repo contains examples, templates, and meeting materials.
 
 A [simple app](visNetwork/simple_editable_saveable/app.R) for creating network diagrams.
+
+(Note: the `.nojekyll` file is intentionally empty and is necessary for creating the website on GitHub. `make_quarto_yml.R` and everything within `.github/` are also needed for rendering the website.)
---
+++ make_quarto_yml.R
@@ -0,0 +1,57 @@
+qmd_files <- list.files(
+  path = ".",
+  pattern = "\\.qmd$",
+  recursive = TRUE,
+  full.names = FALSE
+)
+
+# ignore rendered files and files that start with an underscore
+qmd_files <- qmd_files[
+  !grepl("^docs/", qmd_files) &
+  !grepl("^_", basename(qmd_files)) &
+  qmd_files != "index.qmd"
+]
+
+dirs <- sort(unique(dirname(qmd_files)))
+
+# create static yml contents
+cat(
+'project:
+  type: website
+  output-dir: docs
+
+website:
+  title: "The Pacific Northwest Tribal Coding Group"
+  navbar:
+    left:
+      - href: index.qmd
+        text: Home
+  sidebar:
+    style: docked
+    search: true
+    contents:
+', file = "_quarto.yml"
+)
+
+# append the sections and their contents to the yml file
+for (d in dirs) {
+  section_name <- gsub("/", " / ", d)
+  cat(sprintf('      - section: "%s"\n', section_name), file = "_quarto.yml", append = TRUE)
+  cat('        contents:\n', file = "_quarto.yml", append = TRUE)
+
+  these <- qmd_files[dirname(qmd_files) == d]
+  for (f in these) {
+    cat(sprintf('          - %s\n', f), file = "_quarto.yml", append = TRUE)
+  }
+}
+
+# append more static contents
+cat(
+'
+format:
+  html:
+    theme: cosmo
+',
+  file = "_quarto.yml",
+  append = TRUE
+)
